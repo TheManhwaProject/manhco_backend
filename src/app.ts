9a4b80cc24a0497a64dd099ce2b483d465f5ec21
cy <40525455+rsclip@users.noreply.github.com>
2025-04-05 14:18:14 +0100

    Create server stuff

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,18 @@
-require("module-alias/register");
+import express, { Application } from "express";
+import cors, { CorsOptions } from "cors";
+
+export default class ServerConfig {
+  constructor(app: Application) {
+    this.config(app);
+  }
+
+  private config(app: Application): void {
+    const corsOptions: CorsOptions = {
+      origin: "*", // TODO: CHANGEEE!!!!!!!!!
+    };
+
+    app.use(cors(corsOptions));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+  }
+}
---
+++ src/server.ts
@@ -0,0 +1,20 @@
+require("module-alias/register");
+
+import express, { Application } from "express";
+import ServerConfig from "./app";
+
+const app: Application = express();
+const server: ServerConfig = new ServerConfig(app);
+const PORT: number = process.env.PORT ? parseInt(process.env.PORT, 10) : 8080;
+
+app
+  .listen(PORT, "localhost", function () {
+    console.log(`Server running on port ${PORT}.`);
+  })
+  .on("error", (err: any) => {
+    if (err.code === "EADDRINUSE") {
+      console.log("Port already in use");
+    } else {
+      console.log(err);
+    }
+  });
