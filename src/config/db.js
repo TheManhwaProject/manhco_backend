5a3c1764b1b33fc2ba2f60681c67116b2271bd74
cy <40525455+rsclip@users.noreply.github.com>
2025-04-05 13:24:46 +0100

    Setup database

--- src/config/db.js
+++ src/config/db.js
@@ -1,15 +1,13 @@
-const pgp = require("pg-promise")();
+const { Pool } = require("pg");
 require("dotenv").config();
 
-const db = pgp(process.env.DATABASE_URL);
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
 
-db.connect()
-  .then((obj) => {
-    obj.done();
-    console.log("Connected to database");
-  })
-  .catch((error) => {
-    console.error("Database connection error:", error.message || error);
-  });
+pool.on("error", (err) => {
+  console.error("Unexpected error on idle client", err);
+  process.exit(-1);
+});
 
-module.exports = db;
+module.exports = pool;
