6c9741a5bc9fc2cc5e943200aa1715befacb035d
cy <40525455+rsclip@users.noreply.github.com>
2025-04-07 09:13:29 +0100

    uhh

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,13 @@ import "module-alias/register";
 import express, { Application } from "express";
 import ServerConfig from "./app";
 
+import waitlistRoutes from "@routes/v1/waitlist/waitlistRoutes";
+
 const app: Application = express();
 const server: ServerConfig = new ServerConfig(app);
 const PORT: number = process.env.PORT ? parseInt(process.env.PORT, 10) : 8080;
+
+app.use("/api/v1/waitlist", waitlistRoutes);
 
 app
   .listen(PORT, "localhost", function () {
