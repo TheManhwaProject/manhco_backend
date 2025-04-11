7855608b4d12a07076bd979d65fb763057163d97
cy <40525455+rsclip@users.noreply.github.com>
2025-04-11 11:10:18 +0100

    Add auth routes

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,14 @@ import express, { Application } from "express";
 import ServerConfig from "./app";
 
 import waitlistRoutes from "@routes/v1/waitlist/waitlistRoutes";
+import authRoutes from "@routes/v1/auth/authRoutes";
 
 const app: Application = express();
 const server: ServerConfig = new ServerConfig(app);
 const PORT: number = process.env.PORT ? parseInt(process.env.PORT, 10) : 8080;
 
 app.use("/api/v1/waitlist", waitlistRoutes);
+app.use("/api/v1/auth", authRoutes);
 
 app
   .listen(PORT, "localhost", function () {
