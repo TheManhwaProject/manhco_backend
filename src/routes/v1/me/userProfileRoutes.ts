a97ae4c006e5d8493c044a9db7f77940085cc4d0
cy <40525455+rsclip@users.noreply.github.com>
2025-04-17 10:16:57 +0100

    getUserProfile

--- src/routes/v1/me/userProfileRoutes.ts
+++ src/routes/v1/me/userProfileRoutes.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { profileSetupHandler } from "@controllers/profileSetupController";
+import {
+  profileSetupHandler,
+  getUserProfile,
+} from "@controllers/profileController";
 
 const router = Router();
 
 router.post("/profile-setup", profileSetupHandler);
+router.get("/profile", getUserProfile);
 
 export default router;
