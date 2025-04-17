97ea77db8b5daf871a859a632c798a3f1654ea3d
cy <40525455+rsclip@users.noreply.github.com>
2025-04-17 10:21:40 +0100

    Modification of user profile

--- src/routes/v1/me/userProfileRoutes.ts
+++ src/routes/v1/me/userProfileRoutes.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import {
   profileSetupHandler,
   getUserProfile,
+  editUserProfile,
 } from "@controllers/profileController";
 
 const router = Router();
 
 router.post("/profile-setup", profileSetupHandler);
 router.get("/profile", getUserProfile);
+router.put("/profile", editUserProfile);
 
 export default router;
--- src/types/userProfileSetup.ts
+++ src/types/userProfileSetup.ts
@@ -4,3 +4,12 @@ export const userProfileSetupSchema = z.object({
   username: z.string().min(3).max(32),
   bio: z.string().max(280).optional(),
 });
+
+// For modification
+export const userProfileSchema = z.object({
+  username: z.string().min(3).max(32).optional(),
+  bio: z.string().max(280).optional(),
+  profilePic: z.string().optional(),
+  bannerPic: z.string().optional(),
+  colorTheme: z.string().optional(),
+});
